b49179bb49dca9c8ad9a848a6cb1fb43402e788d
”名前” <>
2020-07-13 06:34:58 +0000

    PHP/Laravel 11 Bladeテンプレートの継承とLaravel Mixの使い方を活用してみよう 課題

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,16 @@
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+{{-- titleに埋め込む --}}
+@section('title', 'Myプロフィール')
+
+{{-- contentに埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection 
