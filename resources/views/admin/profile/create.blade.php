f6ef98dfcfb4afc167a46a52ab0544ac51651929
”名前” <>
2020-07-21 08:23:47 +0000

    PHP/Laravel 14 – tips：データベースについて

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -15,7 +15,7 @@
                     @if(count($errors) > 0)
                         <ul>
                             @foreach($errors->all() as $e)
-                                <li>{{ $e }}</li>
+                                <li><span class="text-danger">{{ $e }}</span></li>
                             @endforeach
                         </ul>
                     @endif
