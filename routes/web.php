0bb7ec97d4aa1a2079aed21905917d77717c41b1
”名前” <>
2020-07-21 11:48:22 +0000

    PHP/Laravel 15 投稿したニュース一覧を表示しよう

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
     Route::post('news/create', 'Admin\NewsController@create');
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth');
 });
 
 Route::group(['prefix' => 'admin'], function() {
