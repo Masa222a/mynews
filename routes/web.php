0db39e50f39db7957e0e448b5e9b77eb1aee7f70
”名前” <>
2020-07-23 11:36:16 +0000

    PHP/Laravel 16 投稿したニュースを更新/削除しよう

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,12 @@ Route::get('/', function () {
 /* Route::get('admin/news/create', 'Admin\NewsController@add');
    グループ化してhttp://XXXXXX.jp/admin/ から始まるURLの設定にしている*/
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-    Route::post('news/create', 'Admin\NewsController@create');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
     Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
+    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
 });
 
 Route::group(['prefix' => 'admin'], function() {
