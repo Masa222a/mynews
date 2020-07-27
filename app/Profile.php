ecb201735b78fb5b8e0fe030b2667180199949b9
”名前” <>
2020-07-27 07:03:40 +0000

    PHP/Laravel 19 一般ユーザーが読むニュースサイトを作成しよう

--- app/Profile.php
+++ app/Profile.php
@@ -14,7 +14,7 @@ class Profile extends Model
             'hobby' => 'required',
             'introduction' => 'required',
         );
-        
+
     //ProfileHistory Modelに関連付けを行う
     public function profile_histories()
     {
